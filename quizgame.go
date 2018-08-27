70a7bafa3538ff9f86b31edba475e0177a437283
Tuan Le <lt.roamer@gmail.com>
2018-08-27 23:23:21 +0700

    add some inputs

--- quizgame.go
+++ quizgame.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+	askName()
+	askAge()
+}
 
+func askName() {
+	fmt.Print("Hi :) Please enter your name here: ")
+	var name string
+	name = runReadString()
+	fmt.Println("Welcome to the game, " + name)
+}
+
+func askAge() {
+	fmt.Print("How old are you: ")
+	//	var age string
+	age := runReadString()
+	//	age = strconv.Atoi(input)
+	fmt.Println("That's great, I always want to be " + age)
+}
+
+func runReadString() string {
+	const inputdelimiter = '\n'
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString(inputdelimiter)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// convert CRLF to LF
+	input = strings.Replace(input, "\n", "", -1)
+	return input
 }
